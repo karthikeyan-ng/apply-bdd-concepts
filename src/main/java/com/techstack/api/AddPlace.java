5ecf389f63195b5fb5694218c521a0d25e2bd6fd
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-03-21 16:48:16 +0530

    Added POJO classes to save place.

---
+++ src/main/java/com/techstack/api/AddPlace.java
@@ -0,0 +1,20 @@
+package com.techstack.api;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+@Data
+public class AddPlace {
+
+    private int accuracy;
+    private String name;
+    private String phone_number;
+    private String address;
+    private String website;
+    private String language;
+    private Location location;
+    private List<String> types;
+}
