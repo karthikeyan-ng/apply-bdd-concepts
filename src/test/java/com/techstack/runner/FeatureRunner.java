5df9f823b5f4a10edfc2409b6527a6d90524497d
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-03-21 18:30:54 +0530

    Added property to run Cucumber in a strict mode.

--- src/test/java/com/techstack/runner/FeatureRunner.java
+++ src/test/java/com/techstack/runner/FeatureRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "classpath:PlacesValidation.feature",
         glue = "classpath:com.techstack.stepdefinitions",
-        plugin = "html:target/cucumber-reports.html"
+        plugin = "html:target/cucumber-reports.html",
+        strict = true
 )
 public class FeatureRunner {
 }
