53fc541bc8a59b0843f0ce30a2beb5cb687a4057
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-03-21 16:54:43 +0530

    Added Json Util class

---
+++ src/main/java/com/techstack/utils/JsonUtils.java
@@ -0,0 +1,37 @@
+package com.techstack.utils;
+
+import io.restassured.path.json.JsonPath;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JsonUtils {
+
+    public static String generateStringFromJsonResource(String fileName) throws IOException {
+
+        Path resourceDirectory = Paths.get("src","test", "resources", "json");
+        Path filePath = Paths.get(resourceDirectory.toString(), fileName);
+        return new String(Files.readAllBytes(filePath));
+    }
+
+    public static JsonPath getJsonPathFromResponse(String response) {
+
+        JsonPath jsonPath = new JsonPath(response);
+        return jsonPath;
+    }
+
+    public static String getValueFromJsonResponse(String response, String path) {
+
+        JsonPath jsonPath = new JsonPath(response);
+        String value = jsonPath.getString(path);
+        return value;
+    }
+
+    public static String getValueFromJsonResponse(JsonPath jsonPath, String path) {
+
+        String value = jsonPath.getString(path);
+        return value;
+    }
+}
